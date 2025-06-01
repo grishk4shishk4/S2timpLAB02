2fce65d7db8d0aa6ac804bc854c7d407c9afc033
grishk4shishk4 <gregoriymukhin@yandex.com>
2025-06-01 12:50:22 +0300

    Add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,10 @@
 
 int main(){
 	std::string name;
+	// создание переменной для хранения имени
 	std::cout << "Enter your name: "; std::cin >> name;
+	// ввод значения переменной
 	std::cout << "hello world from @" << name << endl;
+	// вывод текста на экран
 	return 0;
 }
